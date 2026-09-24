c7d49e9eba8c7fdf616979ce6690477df073540e
Shashank Joshi <sharmashshank385@gmail.com>
2026-09-24 11:37:26 +0530

    Add multiplication functionality

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,12 @@ public class Calculator {
     public static int add(int a, int b) {
         return a + b;
     }
+    public static int multiply(int a, int b) {
+    return a * b;
+}
 
     public static void main(String[] args) {
         System.out.println("Addition: " + add(10, 20));
+        System.out.println("Multiplication: " + multiply(10, 20));
     }
 }
